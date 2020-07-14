8d4fdaaa453864bd8820d2594a54cd1bb242b809
Yifan Yao <yao.740@osu.edu>
2020-07-13 23:29:37 -0400

    View and controller generated for Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # File Edited on 07/13/2020 by Yifan Yao: Device initialized
-  devise_for :users
+  devise_for :users, controllers: {
+      sessions: 'users/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
