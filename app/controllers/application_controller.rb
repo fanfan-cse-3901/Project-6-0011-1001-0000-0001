126ea84d18c305a4942c37a0e1369c0c6993e2d8
Yifan Yao <yao.740@osu.edu>
2020-07-14 02:25:08 -0400

    Home initialized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  # File Edited on 07/13/2020 by Yifan Yao: Device initialized
+  # File Edited on 07/13/2020 by Yifan Yao: Device initialized, users are required to get authenticated in advance
   before_action :authenticate_user!
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  # File Edited on 07/14/2020 by Yifan Yao: Home initialized, as DocumentRoot
+  get 'home/index'
+  root to: 'home#index'
   # File Edited on 07/13/2020 by Yifan Yao: Device initialized
   devise_for :users, controllers: {
-      sessions: 'users/sessions'
+    sessions: 'users/sessions'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
