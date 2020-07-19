0b5a77dcb8ca1eddae6b859375668afd9fe46bfe
Yifan Yao <yao.740@osu.edu>
2020-07-19 19:35:00 -0400

    Link more views into controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   get 'home/index'
   root to: 'home#index'
   # File Edited on 07/13/2020 by Yifan Yao: Device initialized
+  # File Edited on 07/19/2020 by Yifan Yao: Link more views into controllers
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
