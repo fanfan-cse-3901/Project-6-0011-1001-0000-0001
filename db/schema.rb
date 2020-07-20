dfa0aa333361d2395cc435d5e4680af76b392066
kaevondong <dong.696@osu.edu>
2020-07-20 14:57:39 -0400

    Completed migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_14_021300) do
+ActiveRecord::Schema.define(version: 2020_07_20_164144) do
+
+  create_table "items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
