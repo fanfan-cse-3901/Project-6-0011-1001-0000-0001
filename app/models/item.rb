a41969e32899b1a14a1f79b30f13f2b918beb09b
kaevondong <dong.696@osu.edu>
2020-07-26 13:05:56 -0400

    Implemented search function

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
+
+  # Created on 07/25/2020 by Kevin Dong: search method
+  def self.search(search)
+    if search
+      where('title LIKE ?', "%#{search}%")
+    else
+      all
+    end
+  end
 end
