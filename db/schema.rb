528fa62986cfbe67b1ef61488c164a832c9634da
kaevondong <dong.696@osu.edu>
2020-07-26 21:40:39 -0400

    Added search model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_21_053902) do
+ActiveRecord::Schema.define(version: 2020_07_27_004508) do
 
   create_table "carts", force: :cascade do |t|
     t.integer "person_id", null: false
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 2020_07_21_053902) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.string "keywords"
+    t.string "sub_category"
+    t.string "color"
+    t.string "quality"
+    t.string "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
