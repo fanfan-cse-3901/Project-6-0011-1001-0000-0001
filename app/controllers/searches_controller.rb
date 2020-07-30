732c48e7b9779842f2f8607e92e8f2560e0fa48d
kaevondong <dong.696@osu.edu>
2020-07-29 20:59:06 -0400

    Bugfixes for views, globals, user profile

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,7 +8,7 @@ class SearchesController < ApplicationController
     items = items.where('title LIKE ?', "%#{params[:keywords]}%") if params[:keywords] != ''
     items = items.where('color LIKE ?', params[:color]) if params[:color] != 'All'
     items = items.where('quality LIKE ?', params[:quality]) if params[:quality] != 'All'
-    items = items.where('price LIKE ?', params[:price]) if params[:price] != ''
+    items = items.where('price <= ?', params[:price]) if params[:price] != ''
     items = items.where('location LIKE ?', params[:location]) if params[:location] != 'All'
     @items = items
   end
