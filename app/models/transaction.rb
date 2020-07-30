3f21cefdb5dfcfda679fe1811f456303767c65f3
Yifan Yao <yao.740@osu.edu>
2020-07-29 23:51:26 -0400

    Admin cannot change rating

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -6,4 +6,5 @@ class Transaction < ApplicationRecord
   validates :user, presence: true
   validates :rating, inclusion: { in: [1, 2, 3, 4, 5, nil] ,
                                   message: ' is not valid'}
+  validates_uniqueness_of :item_id
 end
--- db/migrate/20200729181844_create_transactions.rb
+++ db/migrate/20200729181844_create_transactions.rb
@@ -1,7 +1,7 @@
 class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
-      t.references :item, foreign_key: true, null: false
+      t.references :item, foreign_key: true, null: false, unique: true
       t.references :user, foreign_key: true, null: false
       t.integer :rating
 
