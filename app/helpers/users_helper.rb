cb5d39831fdb7821a37112f162a25f7bfbf9e774
Yifan Yao <yao.740@osu.edu>
2020-07-31 23:07:11 -0400

    Round user's rate to two digits

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,7 +6,7 @@ module UsersHelper
     get_avg_rating = "SELECT AVG(rating) FROM transactions
                       JOIN items ON transactions.item_id = items.id
                       WHERE items.user_id = #{user_id};"
-    ActiveRecord::Base.connection.execute(get_avg_rating)[0]["AVG(rating)"]
+    ActiveRecord::Base.connection.execute(get_avg_rating)[0]["AVG(rating)"].to_f.round(2)
   end
 
 end
